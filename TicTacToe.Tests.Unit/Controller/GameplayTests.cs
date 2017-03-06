f06b9a8fa854ac53604c5724e890f6f0c129dfae
Gaurav Ravindra <ravindra_gaurav@yahoo.com>
2017-03-06 20:49:43 +0000

    more tests and game logic

--- TicTacToe.Tests.Unit/Controller/GameplayTests.cs
+++ TicTacToe.Tests.Unit/Controller/GameplayTests.cs
@@ -9,20 +9,28 @@ namespace TicTacToe.Tests.Unit.Controller
         [Test]
         public void ExampleGame_P1Wins_AllMarkersDiagonal()
         {
-            // X..
-            // OXO
-            // ..X
-            //P1 Wins
-
             Game game = new Game();
             game.Play("X", 0, 0);
             game.Play("O", 0, 1);
             game.Play("X", 1, 1);
             game.Play("O", 2, 1);
             game.Play("X", 2, 2);
-
             Assert.That(game.GetWinner() == "X");
         }
+
+        [Test]
+        public void ExampleGame_P2Wins_AllMarkersInOneRows()
+        {
+            Game game = new Game();
+            game.Play("X", 2, 1);
+            game.Play("O", 0, 0);
+            game.Play("X", 1, 1);
+            game.Play("O", 0, 1);
+            game.Play("X", 2, 2);
+            game.Play("O", 0, 2);
+            Assert.That(game.GetWinner() == "O");
+        }
+
 
         [Test]
         public void PlayersPlayOnSameField_Exception()
@@ -35,20 +43,46 @@ namespace TicTacToe.Tests.Unit.Controller
         }
 
         [Test]
-        public void SamePlayerPlaysTwice_Exception()
+        public void MarkerAlreadyPlaced_Exception()
         {
             Game game = new Game();
             game.Play("X", 0, 0);
-            game.Play("X", 0, 1);
+            game.Play("X", 1, 0);
+            Assert.Throws<System.InvalidOperationException>(() => game.Play("test", 1, , 1));
+        }
 
+        [Test]
+        public void MakeBoardGeneric()
+        {
             Assert.Inconclusive();
         }
-            
+
+
+        [Test]
+        public void NicerApi()
+        {
+            //Game game = new Game();
+            //Player1.Play(0, 0); //wouldnt need x or o, can just choose that in the start 
+            //game.Play("X", 1, 0);
+            Assert.Inconclusive();
+        }
+
+        [Test]
+        public void NoWinnerYet()
+        {
+            Game game = new Game();
+            game.Play("X", 1, 0);
+            //Assert.IsFalse(GetWinner);
+            Assert.Inconclusive();
+        }
+
 
     }
 
     public class Game
     {
+        private string previousMarker;
+        private string[,] board = new string[3, 3];
         public Game()
         {
         }
@@ -58,9 +92,21 @@ namespace TicTacToe.Tests.Unit.Controller
             return "X";
         }
 
-        public void Play(string marker, int X, int O)
+        public void Play(string marker, int x, int y)
         {
-            
+            if (marker == previousMarker)
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (board[x, y] != null)
+            {
+                throw new InvalidOperationException();
+            }
+
+
+            previousMarker = marker;
+            board[x, y] = marker;
         }
     }
 }
